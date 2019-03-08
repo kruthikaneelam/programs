688c2c4718fca6797fb96802d35dadb326331139
kruthikaneelam <47881181+kruthikaneelam@users.noreply.github.com>
2019-03-08 11:35:54 +0530

    Add files via upload

---
+++ def.py
@@ -0,0 +1,16 @@
+def funct():
+	print("hi")
+	print("hello")
+funct()
+
+#with parameter
+def funct1(a="how are you"):
+	
+	print(a)
+funct1()
+
+#with 2 parameters
+def funct2(a,b,c):
+	k=a+b+c
+	print(k)
+funct2(5,4,7)
